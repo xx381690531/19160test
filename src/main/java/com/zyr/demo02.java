6866cde0172d368b93cf2366d283ea5d4de5ff99
xx381690531 <381690531@qq.com>
2019-09-20 14:37:20 +0800

    secondCommit

---
+++ src/main/java/com/zyr/demo02.java
@@ -0,0 +1,7 @@
+package com.zyr;
+
+public class demo02 {
+    public static void main(String[] args) {
+        System.out.println("hello i am demo02");
+    }
+}
--- src/main/java/com/zyr/testxx.java
+++ src/main/java/com/zyr/testxx.java
@@ -3,7 +3,7 @@ package com.zyr;
 public class testxx {
     public static void main(String[] args) {
         System.out.println("git真好玩");
-        System.out.println("真叫人头大");
+        System.out.println("真叫人头大aaa");
         System.out.println("远程仓库修改代码a");
     }
 }
